00a583ccd2fe4945b0c02281a21d4552722000f9
dvnielnc <djopia863.a@gmail.com>
2024-10-30 23:13:07 -0300

    6ta version login funcionando con base de datos

---
+++ app/src/main/java/com/example/sushipatria/DatabaseHelper.java
@@ -0,0 +1,48 @@
+package com.example.sushipatria;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "SushiPatria.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_USERS = "Usuarios";
+    private static final String COLUMN_USERNAME = "username";
+    private static final String COLUMN_PASSWORD = "password";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_USERS + " (" +
+                COLUMN_USERNAME + " TEXT PRIMARY KEY, " +
+                COLUMN_PASSWORD + " TEXT)";
+        db.execSQL(createTable);
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_USERNAME, "daniel");
+        values.put(COLUMN_PASSWORD, "12345678");
+        db.insert(TABLE_USERS, null, values);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+        onCreate(db);
+    }
+
+    public boolean checkUserCredentials(String username, String password) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT * FROM " + TABLE_USERS + " WHERE " +
+                COLUMN_USERNAME + " = ? AND " + COLUMN_PASSWORD + " = ?";
+        Cursor cursor = db.rawQuery(query, new String[]{username, password});
+        boolean exists = (cursor.getCount() > 0);
+        cursor.close();
+        return exists;
+    }
+}
--- app/src/main/java/com/example/sushipatria/MainActivity.java
+++ app/src/main/java/com/example/sushipatria/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -12,25 +14,41 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private DatabaseHelper databaseHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        databaseHelper = new DatabaseHelper(this);
+
         Button btnIngresar = findViewById(R.id.btnIngresar);
         btnIngresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, activityMenuPrincipal.class);
-                startActivity(intent);
+                String username = editTextUsername.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if (databaseHelper.checkUserCredentials(username, password)) {
+                    Toast.makeText(MainActivity.this, "Inicio De Sesion Exitoso, Bienveni@!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, activityMenuPrincipal.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Credenciales Inválidas", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 }
-
