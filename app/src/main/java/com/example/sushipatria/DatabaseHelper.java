b30841a29962c2493d0dc7c4ec252e489ed2fa3b
dvnielnc <djopia863.a@gmail.com>
2024-11-06 01:44:22 -0300

    Aplicacion terminada con imagenes nuevas

--- app/src/main/java/com/example/sushipatria/DatabaseHelper.java
+++ app/src/main/java/com/example/sushipatria/DatabaseHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "SushiPatria.db";
+    private static final String DATABASE_NAME = "SushiPatria";
     private static final int DATABASE_VERSION = 1;
     private static final String TABLE_USERS = "users";
     private static final String COLUMN_USERNAME = "username";
